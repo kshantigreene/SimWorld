e3cb4064f500a9b3eafedddd504f9c2e1644deea
CloudStrifes01 <raikov806@gmail.com>
2022-10-27 12:18:44 -0400

    Added get functions for human to suit.

--- HumanSim.h
+++ HumanSim.h
@@ -9,6 +9,14 @@ class HumanSim
 public:
 	 HumanSim(string n);
 	 void setWorldInfo(int time, int temp);
+	 double getBattery(); //gets the current battery life
+	 int getSuitWeight(); //gets how much the suit weighs
+		 //suit->getWeight();
+
+	 int getWater(); //gets the current amount of drinkable water
+	 int checkAvailWater(); //gets the current filtration process of the water
+	 
+
 
 private:
 	 string name;
