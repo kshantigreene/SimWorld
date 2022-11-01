a17403ae1e79c42d1ea014c162b59223b73c165d
rodriguek <rodriguek@thomas.edu>
2022-11-01 11:59:04 -0400

    encryption

--- StillsuitSim.cpp
+++ StillsuitSim.cpp
@@ -85,3 +85,108 @@ void StillsuitSim::seperateFluid() {
     }
 }
 
+// Returns gcd of a and b
+int gcd(int a, int h)
+{
+    int temp;
+    while (1) {
+        temp = a % h;
+        if (temp == 0)
+            return h;
+        a = h;
+        h = temp;
+    }
+}
+long encrypt(long msg)
+{
+    // Two random prime numbers
+    srand((unsigned)time(0));
+    long p = (rand() % 8) + 1;
+    bool is_prime = true;
+
+    if (p == 0 || p == 1) {
+        is_prime = false;
+    }
+
+    for (int i = 2; i <= p / 2; ++i)
+    {
+        if (p % i == 0) {
+            is_prime = false;
+            break;
+        }
+    }
+
+    while (is_prime == false)
+    {
+        is_prime = true;
+        p = (rand() % 8) + 1;
+        if (p == 0 || p == 1)
+        {
+            is_prime = false;
+        }
+        for (int i = 2; i <= p / 2; ++i)
+        {
+            if (p % i == 0)
+            {
+                is_prime = false;
+                break;
+            }
+        }
+    }
+    cout << p;
+
+    long q = (rand() % 8) + 1;
+
+    if (q == 0 || q == 1) {
+        is_prime = false;
+    }
+
+    for (int i = 2; i <= q / 2; ++i)
+    {
+        if (q % i == 0) {
+            is_prime = false;
+            break;
+        }
+    }
+
+    while (is_prime == false)
+    {
+        is_prime = true;
+        q = (rand() % 8) + 1;
+        if (q == 0 || q == 1)
+        {
+            is_prime = false;
+        }
+        for (int i = 2; i <= q / 2; ++i)
+        {
+            if (q % i == 0)
+            {
+                is_prime = false;
+                break;
+            }
+        }
+    }
+    cout << q;
+
+    // First part of public key:
+    long n = p * q;
+
+    // Finding other part of public key.
+    // e stands for encrypt
+    long e = 2;
+    long phi = (p - 1) * (q - 1);
+    while (e < phi) {
+        // e must be co-prime to phi and
+        // smaller than phi.
+        if (gcd(e, phi) == 1)
+            break;
+        else
+            e++;
+    }
+    // Encryption c = (msg ^ e) % n
+    long c = pow(msg, e);
+    c = fmod(c, n);
+    //printf("\nEncrypted data = %lf", c);
+    cout << c << endl;
+
+}
