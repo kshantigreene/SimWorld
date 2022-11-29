35706684ad56d2d7d6bcdd382e7c0ddb6cbfeb25
daigled <daigled@AD225-24-PC>
2022-11-29 11:46:01 -0500

    Deleted orginal code

--- HumanSim.cpp
+++ HumanSim.cpp
@@ -23,51 +23,6 @@ void HumanSim::updateHuman(int time,int temp) {
     printf("Current time is: %02d:%02d, %dF\n", hour, minute, temp);
 }
 
-
-
-int HumanSim::calculateInternalTemp(int temp, int InternalTemp) // Function for calculating body temp
-{
-    double activityLevel;
-    activityLevel = calculateActivityLevel();
-    InternalTemp += activityLevel * 2;
-
-    if(temp < 60)
-    {
-        InternalTemp -= 2;
-
-    }else if(temp >= 60 && temp < 70)
-    {
-        InternalTemp -= 1;
-
-    }
-    else if (temp >  70 && temp <= 80) 
-    {
-        InternalTemp += 1;
-    }
-    else
-    {
-        InternalTemp += 2;
-    }
-    return InternalTemp;
-
-    
-}
-
-
-
-
-
-    // getters and setters for Internal Temo
-void HumanSim::setInternalTemp(int InternalTemp) // Setter for Internal temp
-{
-    this->internalTemp = InternalTemp; // base body temp
-}
-
-int HumanSim::getInternalTemp() // getter for Internal temp 
-{
-    return this->internalTemp;
-}
-
 void HumanSim::HumanLocation()
 {
    
