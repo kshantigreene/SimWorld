b02b8ac09ceff7566be6d011927ceec346ec31e5
hollowellm <hollowellm@AD225-23-PC>
2022-11-29 11:31:20 -0500

    added michael cpp

---
+++ Michael.cpp
@@ -0,0 +1,16 @@
+#include "HumanSim.h"
+#include "StillsuitSim.h"
+
+void HumanSim::HumanLocation()
+{
+
+
+    if (usingSuit)
+    {
+        //call suit to tell it whether inside  or outside 
+    }
+
+
+
+}
+
