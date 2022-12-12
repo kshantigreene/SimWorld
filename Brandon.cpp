1266fbcb9189f1f5230a622ac39bcd205220c44b
Brandon Hough <houghb@thomas.edu>
2022-12-12 15:03:59 -0500

    batteryLevel change

--- Brandon.cpp
+++ Brandon.cpp
@@ -7,21 +7,25 @@ using namespace std;
 
 double StillsuitSim::batteryLevel(int time, double currentActivityLevel)
 {
+    while (inSuit == true)
+    {
 
-    //assigns values to variables
-    int batteryLevel = 100;
-    int maxBatteryTime = 720;
 
-    //uses worldtime to find how many minuites are left in the battery
-    int batteryTimeLeft;
-    batteryTimeLeft = maxBatteryTime - worldTime;
+        //assigns values to variables
+        int batteryLevel = 100;
+        int maxBatteryTime = 720;
 
-    // every minute called, battery level lowers by 1 minute
-    battery = (1 - currentActivityLevel);
+        //uses worldtime to find how many minuites are left in the battery
+        int batteryTimeLeft;
+        batteryTimeLeft = maxBatteryTime - worldTime;
 
-    if (battery < 15) {
-        cout << "Battery is low." << endl;
+        // every minute called, battery level lowers by 1 minute
+        battery = (1 - currentActivityLevel);
+
+        if (battery < 15) {
+            cout << "Battery is low." << endl;
+        }
+
+        return battery;
     }
-
-    return battery;
 }
